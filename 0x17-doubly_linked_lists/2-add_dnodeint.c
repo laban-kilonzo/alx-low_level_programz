307fc7bab3c1d437b7f997f6a7a5264f1d884ce6
laban-kilonzo <labanmutisya7@gmail.com>
2022-12-08 23:29:25 -0800

    coding

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
+ * add_dnodeint - Adds a new node at the beginning
+ * of a dlistint_t list.
  * @head: A pointer to the head of the dlistint_t list.
  * @n: The integer for the new node to contain.
  *
