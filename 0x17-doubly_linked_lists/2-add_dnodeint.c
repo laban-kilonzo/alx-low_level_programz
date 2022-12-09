43f0d23e607653ceca54f1bbe1ccedefc08f8c3d
laban-kilonzo <labanmutisya7@gmail.com>
2022-12-09 02:55:56 -0800

    coding

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,8 +6,8 @@
  * @head: A pointer to the head of the dlistint_t list.
  * @n: The integer for the new node to contain.
  *
- * Return: If the function fails - NULL.
- * Otherwise - the address of the new node.
+ * Return: If the function fails - NULL.Otherwise - the
+ * address of the new node.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
