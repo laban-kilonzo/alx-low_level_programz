2346ad3c1d4d2712b1b20ac4c70249944dbbf412
laban-kilonzo <labanmutisya7@gmail.com>
2022-12-13 01:47:10 -0800

    coding

--- 0-read_textfile.c/0-read_textfile.c
+++ 0-read_textfile.c/0-read_textfile.c
@@ -1,101 +1,35 @@
 #include "main.h"
-
 #include <stddef.h>
 
-
-
 /**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file
+ * @letters:  number of letters to be printed
  *
- *  * _strlen - counts string length
- *
- *   * @str: string to be used
- *
- *    *
- *
- *     * Return: length of the string
- *
- *      */
-
-int _strlen(char *str)
-
+ * Return: number of letters read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int file, n_read, wrote;
+	char *buffer;
 
-		int len = 0;
-
-
-
-			while (str[len] != '\0')
-
-						len++;
-
-				return (len);
-
-}
-
-
-
-/**
- *
- *  * create_file - creates a file
- *
- *   * @filename: name of the file
- *
- *    * @text_content: content of the file to be created
- *
- *     *
- *
- *      * Return: 1 on success, -1 otherwise
- *
- *       */
-
-int create_file(const char *filename, char *text_content)
-
-{
-
-		int file, wrote;
-
-
-
-			if (filename == NULL)
-
-						return (-1);
-
-				file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
-					if (file == -1)
-
-								return (-1);
-
-						if (text_content != NULL)
-
-								{
-
-											wrote = write(file, text_content, _strlen(text_content));
-
-													if (wrote == -1)
-
-																{
-
-																				close(file);
-
-																							return (-1);
-
-																									}
-
-															close(file);
-
-																	return (1);
-
-																		}
-
-							else
-
-									{
-
-												close(file);
-
-														return (1);
-
-															}
-
+	buffer = malloc(sizeof(*buffer) * (letters + 1));
+	if (filename == NULL || buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	n_read = read(file, buffer, letters);
+	if (n_read == -1)
+		return (0);
+	buffer[n_read] = '\0';
+	wrote = write(STDOUT_FILENO, buffer, n_read);
+	if (wrote != n_read)
+		return (0);
+	free(buffer);
+	close(file);
+	return (n_read);
 }
